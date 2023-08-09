97d0387dcf1665d0842b8b29fdca39bd8520f515
yklee <yklee@xprime.co.kr>
2023-08-09 16:29:24 +0900

    Initial commit

---
+++ src/main/kotlin/1_Variable.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+
+    // 변수 선언
+    val a: Int = 1
+
+    // 타입 생략하고 선언가능
+    var b = 1
+
+    // 지연 할당 (타입선언 필수)
+    val c: Int
+    c = 3
+
+    // val(value)
+    // java 의 final 키워드와 유사
+
+    // var(variable)
+    // 재할당 가능
+
+
+}
